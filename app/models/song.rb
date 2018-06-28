af926ed8dca2a70a26fad15814843d943e22356d
noah-berman <noahberm@gmail.com>
2018-06-28 17:13:42 -0400

    complete models, add controllers for genres and songs

---
+++ app/models/song.rb
@@ -0,0 +1,19 @@
+class Song < ActiveRecord::Base
+  belongs_to :artist
+  has_many :song_genres
+  has_many :genres, through: :song_genres
+
+  def slug
+    unslugged = self.name
+    slugged = unslugged.gsub(/\s+/&&/[^\w]/,"-").downcase
+    slugged
+  end
+
+  def self.find_by_slug(slug)
+    Song.all.find do |song|
+      song.slug == slug
+    end
+  end
+
+
+end
